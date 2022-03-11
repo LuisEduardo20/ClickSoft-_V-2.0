f27b5e33c6f229ba74f4a1910df8616863bb8ee0
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-11 15:58:19 -0300

    Adição da responsividade

--- web/src/pages/Main/styles.ts
+++ web/src/pages/Main/styles.ts
@@ -3,10 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: var(--yellow);
 
   display: grid;
   grid-template-columns: 25% 1fr;
   grid-template-rows: 1fr;
   grid-template-areas: "UsersList MainArea";
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 2fr 1fr;
+    grid-template-areas: "MainArea" "UsersList";
+  }
 `;
