f47fb6a842d3eb86bd77a669361b09a51bc64d1a
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-11 15:56:43 -0300

    Componente principal de exibição do usuário

---
+++ web/src/components/MainSection/index.tsx
@@ -0,0 +1,15 @@
+import SearchHeader from "../SearchHeader";
+import UserCard from "../UserCard";
+
+import { Container } from "./styles";
+
+const SearchUser = () => {
+  return (
+    <Container>
+      <SearchHeader />
+      <UserCard />
+    </Container>
+  );
+};
+
+export default SearchUser;
