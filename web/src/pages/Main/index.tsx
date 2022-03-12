273a9ca75cfd5f6f0ee5ce72924088f025afaaf0
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-11 23:20:50 -0300

    Adição do Modal

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -2,12 +2,15 @@ import SearchedUsersList from "../../components/SearchedUsersList";
 import SearchUser from "../../components/MainSection";
 
 import { Container } from "./styles";
+import UserModal from "../../components/UserModal";
 
 const Main = () => {
   return (
     <Container>
       <SearchedUsersList />
       <SearchUser />
+
+      <UserModal />
     </Container>
   );
 };
