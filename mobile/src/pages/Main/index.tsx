92e0ae228361831f8df7af797efd147aefe1a3f7
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-03-13 13:13:10 -0300

    Adição da lista de usuários pesquisados

--- mobile/src/pages/Main/index.tsx
+++ mobile/src/pages/Main/index.tsx
@@ -3,20 +3,25 @@ import React from "react";
 
 import SearchHeader from "../../components/SearchHeader";
 import UserCard from "../../components/UserCard";
+import SearchedUsers from "../../components/SearchedUsers";
 
 const Main = () => {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scroll}>
         <SearchHeader />
         <UserCard />
-      </View>
-    </ScrollView>
+        <SearchedUsers />
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  scroll: {
     flex: 1,
   },
 });
